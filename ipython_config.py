908d1dde62b5c3d818adf9bc1b9df6f49822c69b
Nick Tallant <30448510+ndtallant@users.noreply.github.com>
2017-11-19 14:08:51 -0600

    Update ipython_config.py
    Fixed typo

--- ipython_config.py
+++ ipython_config.py
@@ -2,7 +2,7 @@
 
 c.InteractiveShell.colors = 'Linux'
 c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+c.InteractiveShellApp.exec_lines = ['autoreload 2']
 c.InteractiveShellApp.exec_lines = ['import pandas as pd']
 c.InteractiveShellApp.exec_lines = ['import numpy as np']
 
