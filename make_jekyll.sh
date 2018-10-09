92f0e6978d656b92e2b28991082688fedbe53395
Nick Tallant <ndtallant@uchicago.edu>
2018-10-08 19:15:02 -0500

    sass note for later

--- make_jekyll.sh
+++ make_jekyll.sh
@@ -2,3 +2,15 @@ apt-get install ruby
 apt-get install ruby-dev
 apt-get install ruby`ruby -e 'puts RUBY_VERSION[/\d+\.\d+/]'`-dev
 gem install bundler jekyll
+
+################
+# Ruby Sass is deprecated and will be unmaintained as of 26 March 2019.
+
+# * If you use Sass as a command-line tool, we recommend using Dart Sass, the new
+#   primary implementation: https://sass-lang.com/install
+
+# * If you use Sass as a plug-in for a Ruby web framework, we recommend using the
+#   sassc gem: https://github.com/sass/sassc-ruby#readme
+
+# * For more details, please refer to the Sass blog:
+#   http://sass.logdown.com/posts/7081811
