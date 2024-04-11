116e7fcadad9492d5e9b2ff811bd5d061be8881f
Nick Tallant <ndtallant@gmail.com>
2024-04-10 21:40:24 -0500

    Use pyright LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,10 +1,25 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ft = { "go" },
+    ft = { "go", "python" },
     config = function()
       local lspconfig = require "lspconfig"
       lspconfig.gopls.setup {}
+      lspconfig.pyright.setup {}
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        end,
+      })
     end,
   },
 }
