87c728084fcaa38f0bbd14e9a1fb6d142ab23c7e
Nick Tallant <ndtallant@gmail.com>
2024-12-02 13:12:49 -0600

    Make wezterm look nicer (no bar)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,12 @@
 local wezterm = require("wezterm")
 
 return {
-	font_size = 15.5,
+	font_size = 16.5,
 	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
 	tab_bar_at_bottom = true,
-	color_scheme = "terafox",
+	color_scheme = "nightfox",
 	hide_tab_bar_if_only_one_tab = true,
+	window_background_opacity = 0.75,
+	macos_window_background_blur = 25,
+	window_decorations = "RESIZE",
 }
